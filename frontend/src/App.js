658562a6863bb9c081ae2505cdfd0a09c04fce8f
Soslan <sparksoslan@gmail.com>
2020-11-07 18:41:28 +0000

    HomeScreen component - Product Listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <main className="py-3">
         <Container>
           <h1>Welcome</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="#home">E-commerce</Navbar.Brand>
+          <Navbar.Brand href="/">E-commerce</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
