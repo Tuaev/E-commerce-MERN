78b763a99c3f9e0c8f5259e83eb41bb86152339a
Soslan <sparksoslan@gmail.com>
2020-11-08 15:58:46 +0000

    Add mongoose and connect mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
-const express = require('express');
-const products = require('./data/products');
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import products from './data/products.js';
 
+dotenv.config();
+connectDB();
 const app = express();
 
 app.get('/', (req, res) => {
@@ -16,4 +20,5 @@ app.get('/api/products/:id', (req, res) => {
   res.json(products[product]);
 });
 
-app.listen(5000, console.log('Server running on port 5000'));
+const PORT = process.env.PORT;
+app.listen(PORT, console.log(`Server running |${process.env.NODE_ENV}| mode on port ${PORT}`));
