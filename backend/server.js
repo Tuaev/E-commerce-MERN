53b465cb3029f44204feebc3467acde8aaf34a4d
Soslan <sparksoslan@gmail.com>
2020-11-08 20:47:44 +0000

    Add Backend - Fetch Product from DB

--- backend/server.js
+++ backend/server.js
@@ -2,26 +2,19 @@ import express from 'express';
 import dotenv from 'dotenv';
 import 'colors';
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
 connectDB();
 const app = express();
+const PORT = process.env.PORT;
 
 app.get('/', (req, res) => {
   res.send('API /');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.findIndex((product) => product._id === req.params.id);
-  res.json(products[product]);
-});
-
-const PORT = process.env.PORT;
 app.listen(
   PORT,
   console.log(`Server running |${process.env.NODE_ENV}| mode on port ${PORT}`.brightYellow)
