0afff3572ee408261e4e21a710777ef2d4c39e77
Soslan <sparksoslan@gmail.com>
2020-11-08 18:42:26 +0000

    Add Initial DB seed data and script

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # E-commerce-MERN
+
+
+### Config:
+  - Change ".env copy" file to your own values and rename to ".env"
+  - Seed Database with `npm run data:import`
---
+++ backend/data/users.js
@@ -0,0 +1,21 @@
+import bcrypt from 'bcryptjs';
+const users = [
+  {
+    name: 'Admin user',
+    email: 'admin@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'John Doe',
+    email: 'john@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+  {
+    name: 'Jane Smith',
+    email: 'jane@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+];
+
+export default users;
