8cf40477a33ddf91f3facdcc07287ef977472f35
Soslan <sparksoslan@gmail.com>
2020-11-09 14:36:56 +0000

    Add Backend -  error handler middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -22,12 +22,12 @@ router.get(
   asyncHandler(async (req, res, next) => {
     const product = await Product.findById(req.params.id);
 
-    // if (product) {
-    //   res.json(product);
-    // } else {
-    //   res.status(404).json({ message: 'Product not found' });
-    // }
-    product ? res.json(product) : res.status(404).json({ message: 'Product not found' });
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404);
+      throw new Error('Product not found');
+    }
   })
 );
 
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import 'colors';
 import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
@@ -14,6 +15,8 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(
   PORT,
