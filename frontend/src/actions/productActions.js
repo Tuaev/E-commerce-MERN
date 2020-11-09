a2b61166417b5e2e5dbad3f8d6cae89ad0b57203
Soslan <sparksoslan@gmail.com>
2020-11-09 17:36:32 +0000

    Add fetch homescreen data using redux dispatch and selector

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -8,7 +8,6 @@ import {
 export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
-
     const { data } = await axios.get('/api/products');
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
   } catch (err) {
