56b44c54d060c580d455ed68ab6f38257d1373ed
Soslan <sparksoslan@gmail.com>
2020-11-09 21:13:39 +0000

    Add product details action and reducer

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,6 +3,9 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from '../constants/productConstants';
 
 export const listProducts = () => async (dispatch) => {
@@ -14,5 +17,17 @@ export const listProducts = () => async (dispatch) => {
     const payloadErrorMessage =
       err.response && err.response.data.message ? err.response.data.message : err.message;
     dispatch({ type: PRODUCT_LIST_FAIL, payload: payloadErrorMessage });
+  }
+};
+
+export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
+    const { data } = await axios.get(`/api/products/${id}`);
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+  } catch (err) {
+    const payloadErrorMessage =
+      err.response && err.response.data.message ? err.response.data.message : err.message;
+    dispatch({ type: PRODUCT_DETAILS_FAIL, payload: payloadErrorMessage });
   }
 };
