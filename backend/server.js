74f5293499be694d6dd8fbd0e44887201213fcd9
Soslan <sparksoslan@gmail.com>
2020-11-10 16:22:31 +0000

    Add user authentication endpoint and password match

--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,21 @@ import 'colors';
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 connectDB();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('API /');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
